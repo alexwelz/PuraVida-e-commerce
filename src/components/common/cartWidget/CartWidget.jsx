d3bb945f7c9f53683f3809080ed841ae0789e6b1
Alex Gustavo Welz <alex.welz6@gmail.com>
2025-01-13 15:42:42 -0300

    Agrego burbuja con valor estatico

--- src/components/common/cartWidget/CartWidget.jsx
+++ src/components/common/cartWidget/CartWidget.jsx
@@ -1,8 +1,11 @@
 import { FaCartShopping } from "react-icons/fa6";
+import "./cartWidget.css";
+
 export const CartWidget = () => {
   return (
-    <div>
-      <FaCartShopping />
+    <div className="cart-widget">
+      <FaCartShopping className="cart-icon" />
+      <div className="bubble">0</div>
     </div>
   );
 };
