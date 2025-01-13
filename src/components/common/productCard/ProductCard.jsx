d10e65596d5532bd7aa3a800ed71887b23bd122a
Alex Gustavo Welz <alex.welz6@gmail.com>
2025-01-13 18:34:13 -0300

    Agrego mensaje de bienvenida

--- src/components/common/productCard/ProductCard.jsx
+++ src/components/common/productCard/ProductCard.jsx
@@ -1,15 +1,24 @@
-import "./productCard.css";
+// import "./productCard.css";
+
+// export const ProductCard = (props) => {
+//   console.log(props);
+//   return (
+//     <div className="product">
+//       <h2>{props.title}</h2>
+//       <h2>{props.price}</h2>
+//       <h2>{props.description}</h2>
+//     </div>
+//   );
+// };
+
+import "./ProductCard.css";
 
 export const ProductCard = (props) => {
-  console.log(props);
   return (
-    <div className="product">
-      <h2>{props.title}</h2>
-      <h2>{props.price}</h2>
-      <h2>{props.description}</h2>
-      <p>
-        <strong>Precio $</strong>
-      </p>
+    <div className="product-card">
+      <h3>{props.title}</h3>
+      {props.price && <p>Precio: {props.price}</p>}
+      <p>{props.description}</p>
     </div>
   );
 };
--- src/components/pages/itemListContainer/ItemListContainer.jsx
+++ src/components/pages/itemListContainer/ItemListContainer.jsx
@@ -1,24 +1,42 @@
+// import "./itemListContainer.css";
+// import { ProductCard } from "../../common/productCard/ProductCard";
+
+// const ItemListContainer = () => {
+//   return (
+//     <div>
+//       <h2>Productos</h2>
+//       <ProductCard
+//         title="Titulo 1"
+//         price="precio 1"
+//         description="descripcion 1"
+//       />
+//       <ProductCard
+//         price="precio 2"
+//         title="titulo 2"
+//         description="descripcion 2"
+//       />
+//       <ProductCard
+//         title="titulo 3"
+//         price="precio 3"
+//         description="descripcion 3"
+//       />
+//     </div>
+//   );
+// };
+
+// export default ItemListContainer;
+
 import "./itemListContainer.css";
 import { ProductCard } from "../../common/productCard/ProductCard";
 
 const ItemListContainer = () => {
   return (
     <div>
-      <h2>Productos</h2>
+      <h2>Bienvenido a Pura Vida</h2>
       <ProductCard
-        title="Titulo 1"
-        price="precio 1"
-        description="descripcion 1"
-      />
-      <ProductCard
-        price="precio 2"
-        title="titulo 2"
-        description="descripcion 2"
-      />
-      <ProductCard
-        title="titulo 3"
-        price="precio 3"
-        description="descripcion 3"
+        title="Bienvenido a mi tienda!"
+        price=""
+        description="Aquí encontrarás los mejores productos y precios del mercado de surf y surfskate. No te lo pierdas!"
       />
     </div>
   );
