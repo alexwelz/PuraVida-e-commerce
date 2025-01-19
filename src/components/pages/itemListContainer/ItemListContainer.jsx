4401a5b2a6cc39dfb961ac8244990176e995f7d7
Alex Gustavo Welz <alex.welz6@gmail.com>
2025-01-19 12:13:05 -0300

    Agrego productos cards

--- src/components/pages/itemListContainer/ItemListContainer.jsx
+++ src/components/pages/itemListContainer/ItemListContainer.jsx
@@ -1,42 +1,55 @@
-// import "./itemListContainer.css";
-// import { ProductCard } from "../../common/productCard/ProductCard";
-
-// const ItemListContainer = () => {
-//   return (
-//     <div>
-//       <h2>Productos</h2>
-//       <ProductCard
-//         title="Titulo 1"
-//         price="precio 1"
-//         description="descripcion 1"
-//       />
-//       <ProductCard
-//         price="precio 2"
-//         title="titulo 2"
-//         description="descripcion 2"
-//       />
-//       <ProductCard
-//         title="titulo 3"
-//         price="precio 3"
-//         description="descripcion 3"
-//       />
-//     </div>
-//   );
-// };
-
-// export default ItemListContainer;
-
 import "./itemListContainer.css";
 import { ProductCard } from "../../common/productCard/ProductCard";
+import { products } from "../../products";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const ItemListContainer = () => {
+  //Simula una peticion que me devuelva los productos
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    const getProducts = new Promise((resolve, reject) => {
+      const isLogged = true;
+
+      if (isLogged) {
+        resolve(products);
+      } else {
+        reject({ statusCode: 400, message: "Error, algo salio mal" });
+      }
+    });
+
+    getProducts
+      .then((response) => {
+        setItems(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {});
+  }, []);
+
   return (
     <div>
       <h2>Bienvenido a Pura Vida</h2>
       <ProductCard
-        title="Bienvenido a mi tienda!"
-        price=""
-        description="Aquí encontrarás los mejores productos y precios del mercado de surf y surfskate. No te lo pierdas!"
+        title={items[0]?.title}
+        price={items[0]?.price}
+        description={items[0]?.description}
+      />
+      <ProductCard
+        title={items[1]?.title}
+        price={items[1]?.price}
+        description={items[1]?.description}
+      />
+      <ProductCard
+        title={items[2]?.title}
+        price={items[2]?.price}
+        description={items[2]?.description}
+      />
+      <ProductCard
+        title={items[3]?.title}
+        price={items[3]?.price}
+        description={items[3]?.description}
       />
     </div>
   );
---
+++ src/components/products.js
@@ -0,0 +1,46 @@
+export let products = [
+  {
+    id: "1",
+    title: "Carver C7 Raw 30,75' Thunder",
+    price: 300125,
+    description:
+      "El Thunder presenta una concavidad profunda con patadas en la punta y la cola adecuadas para el entrenamiento avanzado de surf y el uso en skatepark. Lleva tu surf al siguiente nivel y diviértete al máximo.",
+    imgURL:
+      "https://res.cloudinary.com/dcykysgy9/image/upload/v1737068915/1__41827.1721951166_tnbghq.jpg",
+    stock: 2,
+    category: "Tipo de eje",
+  },
+  {
+    id: "2",
+    title: "Carver CX Raw 29,25' Bloom",
+    price: 306250,
+    description:
+      "Una tabla rápida y ágil, la cubierta ancha y la punta en forma de cuchara realmente capturan la sensación de montar una Fish clásica en una ola épica y cristalina.",
+    imgURL:
+      "https://res.cloudinary.com/dcykysgy9/image/upload/v1737068976/1__39384.1722001530_g1svsf.jpg",
+    stock: 12,
+    category: "Tamaño de la tabla",
+  },
+  {
+    id: "3",
+    title: "Tabla de surf completa Carver C5 32'' Omni",
+    price: 243775,
+    description:
+      "La plantilla más estrecha de 8,5” la mantiene liviana, con un poco más de espacio en la punta para un giro más amplio. La Omni viene de serie con ruedas Carver Park negras de 58 mm x 95a diseñadas con un perfil redondeado que ayuda a bloquearse en los grinds, ejes huecos C5 y cojinetes integrados.",
+    imgURL:
+      "https://res.cloudinary.com/dcykysgy9/image/upload/v1737068998/Carver2024_Omni_C5__51096.1713939364_lh4vy5.jpg",
+    stock: 12,
+    category: "Ruedas",
+  },
+  {
+    id: "4",
+    title: "Tabla de surf Carver Swallow 29,5' 2022 C7",
+    price: 300125,
+    description:
+      "Al igual que su homónimo, esta tabla es rápida y suelta, una combinación mágica de largo y ancho que hace que sea fácil de bombear.",
+    imgURL:
+      "https://res.cloudinary.com/dcykysgy9/image/upload/v1737069025/CarverSwallow2022C7__97455.1686072704_ffkubb.jpg",
+    stock: 1,
+    category: "Ruedas",
+  },
+];
