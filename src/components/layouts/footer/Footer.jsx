8cb5d2eadd28a750b2d63f49c8f2848f9f55274b
Alex Gustavo Welz <alex.welz6@gmail.com>
2025-02-04 21:46:34 -0300

    Mejoras footer

--- src/components/layouts/footer/Footer.jsx
+++ src/components/layouts/footer/Footer.jsx
@@ -1,5 +1,20 @@
 import "./footer.css";
+import { IoLogoGithub } from "react-icons/io5";
+import { TbBrandLinkedinFilled } from "react-icons/tb";
+import { Link } from "react-router-dom";
 
 export const Footer = () => {
-  return <h3 className="footer">Footer</h3>;
+  return (
+    <div className="footer">
+      <h3>Contactos</h3>
+      <div className="icons">
+        <Link to={"https://www.linkedin.com/in/alex-gustavo-welz/"}>
+          <TbBrandLinkedinFilled />
+        </Link>
+        <Link to={"https://github.com/alexwelz/PuraVida-e-commerce"}>
+          <IoLogoGithub />
+        </Link>
+      </div>
+    </div>
+  );
 };
