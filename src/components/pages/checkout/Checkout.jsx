55216e4352a739b88a719ee249dc70a1fbbb6d4a
Alex Gustavo Welz <alex.welz6@gmail.com>
2025-02-23 19:01:04 -0300

    Agrego logica orden de compra

--- src/components/pages/checkout/Checkout.jsx
+++ src/components/pages/checkout/Checkout.jsx
@@ -1,6 +1,11 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { CartContext } from "../../../context/cart/CartContext";
+import { db } from "../../../firebaseConfig";
+import { addDoc, collection } from "firebase/firestore";
 
 export const Checkout = () => {
+  const { cart, getTotalAmount } = useContext(CartContext);
+  const [orderTicket, setOrderTicket] = useState(null);
   const [userInfo, setUserInfo] = useState({
     name: "",
     phone: "",
@@ -8,8 +13,20 @@ export const Checkout = () => {
   });
 
   const buy = (event) => {
-    event.preventDefualt();
+    event.preventDefault();
     console.log(userInfo);
+
+    //Orden de compra desde firestore
+    let total = getTotalAmount();
+    let order = {
+      buyer: userInfo,
+      items: cart,
+      total: total,
+    };
+
+    let orderCollection = collection(db, "orders");
+    const newOrder = addDoc(orderCollection, order);
+    newOrder.then((res) => setOrderTicket(res.id));
   };
 
   const caprutreData = (event) => {
@@ -19,28 +36,34 @@ export const Checkout = () => {
   };
   return (
     <div>
-      <h2>Formulario de compra</h2>
-      <form onSubmit={buy}>
-        <input
-          type="text"
-          placeholder="Nombre"
-          name="name"
-          onChange={caprutreData}
-        />
-        <input
-          type="text"
-          placeholder="Telefono"
-          name="phone"
-          onChange={caprutreData}
-        />
-        <input
-          type="text"
-          placeholder="Email"
-          name="email"
-          onChange={caprutreData}
-        />
-        <button>Comprar</button>
-      </form>
+      {orderTicket ? (
+        <h1 id="modalOrder">Tu comprobante de comra es: {orderTicket}</h1>
+      ) : (
+        <div>
+          <h2 className="titleForm">Formulario de compra</h2>
+          <form onSubmit={buy} className="orderForm">
+            <input
+              type="text"
+              placeholder="Nombre"
+              name="name"
+              onChange={caprutreData}
+            />
+            <input
+              type="text"
+              placeholder="Telefono"
+              name="phone"
+              onChange={caprutreData}
+            />
+            <input
+              type="text"
+              placeholder="Email"
+              name="email"
+              onChange={caprutreData}
+            />
+            <button id="buyOrder">Comprar</button>
+          </form>
+        </div>
+      )}
     </div>
   );
 };
