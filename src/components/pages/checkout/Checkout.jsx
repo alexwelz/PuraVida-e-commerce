e6b5d2bc2878ee9312b40b55d1f9e81a2d9836e3
Alex Gustavo Welz <alex.welz6@gmail.com>
2025-02-23 20:09:49 -0300

    Agrego disenio formulario de compra

--- src/components/pages/checkout/Checkout.jsx
+++ src/components/pages/checkout/Checkout.jsx
@@ -1,10 +1,11 @@
 import { useContext, useState } from "react";
+import "./checkout.css";
 import { CartContext } from "../../../context/cart/CartContext";
 import { db } from "../../../firebaseConfig";
 import { addDoc, collection, updateDoc, doc } from "firebase/firestore";
 
 export const Checkout = () => {
-  const { cart, getTotalAmount } = useContext(CartContext);
+  const { cart, getTotalAmount, removeCart } = useContext(CartContext);
   const [orderTicket, setOrderTicket] = useState(null);
   const [userInfo, setUserInfo] = useState({
     name: "",
@@ -26,7 +27,10 @@ export const Checkout = () => {
     //Guardar orden en firestore
     let orderCollection = collection(db, "orders");
     const newOrder = addDoc(orderCollection, order);
-    newOrder.then((res) => setOrderTicket(res.id));
+    newOrder.then((res) => {
+      setOrderTicket(res.id);
+      removeCart();
+    });
 
     let productsCollection = collection(db, "products");
 
@@ -48,27 +52,32 @@ export const Checkout = () => {
       ) : (
         <div>
           <h2 className="titleForm">Formulario de compra</h2>
-          <form onSubmit={buy} className="orderForm">
-            <input
-              type="text"
-              placeholder="Nombre"
-              name="name"
-              onChange={caprutreData}
-            />
-            <input
-              type="text"
-              placeholder="Telefono"
-              name="phone"
-              onChange={caprutreData}
-            />
-            <input
-              type="text"
-              placeholder="Email"
-              name="email"
-              onChange={caprutreData}
-            />
-            <button id="buyOrder">Comprar</button>
-          </form>
+          <div className="container">
+            <form onSubmit={buy} className="formGroup">
+              <input
+                type="text"
+                placeholder="Nombre"
+                name="name"
+                onChange={caprutreData}
+                className="formInput"
+              />
+              <input
+                type="text"
+                placeholder="Telefono"
+                name="phone"
+                onChange={caprutreData}
+                className="formInput"
+              />
+              <input
+                type="text"
+                placeholder="Email"
+                name="email"
+                onChange={caprutreData}
+                className="formInput"
+              />
+              <button id="buyOrder">Comprar</button>
+            </form>
+          </div>
         </div>
       )}
     </div>
